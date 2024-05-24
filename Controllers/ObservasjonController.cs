69cf737461715ac3e4af6e9007cc2216d8b63837
Gruppe5-FullStack <gruppe5fullstack@gmail.com>
2024-05-24 03:51:51 +0200

    Del 2: Deloppgave 7a
    <Lag ny side for temperatur målinger fra FrostAPI>
    
    - Laget ny side ved bruk av scaffolding teknikken.
    
    - Slettet unødvendige filer som Create.cshtml, Delete.cshtml osv. samt deres kode i kontrolleren som ble automatisk generert under scaffoldingen.
    
    Deloppgave 7a er ferdig

--- Controllers/ObservasjonController.cs
+++ Controllers/ObservasjonController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Gruppe5.Data;
 
 namespace Gruppe5.Controllers
 {
     public class ObservasjonController : Controller
     {
-        public IActionResult Index()
+        private readonly Gruppe5Context _context;
+
+        public ObservasjonController(Gruppe5Context context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            // Vis data fra 7 siste dager
+            var observasjoner = await _context.Observation
+                .OrderByDescending(o => o.Date)
+                .Take(7)
+                .ToListAsync();
+
+            return View("~/Views/Observation/Index.cshtml", observasjoner); // localhost:7054/Observasjon
+        }
+
     }
 }
