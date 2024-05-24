ecce97c642d3c117d327905f484f7a56bb426db9
Gruppe5-FullStack <gruppe5fullstack@gmail.com>
2024-05-24 03:31:24 +0200

    Del 1: Deloppgave 4
    <Opprett database for lagring av Vær data>
    
    - Slettet WeatherForecast katalogen samt dens innhold. Dette ble gjort for å unngå mulige komplikasjoner under utføring av scaffolding.
    
    - Installert Microsoft.EntityFrameworkCore.Design versjon 8.0.4 ved bruk av "Tools --> Manage NuGet Packages for Solution...". Dette ble gjort for å utføre scaffolding teknikken i Visual Studio ved bruk av rammeverket MVC.
    
    Scaffolding oppsett og utføring:
    Venstre klikk på katalogen "Controllers" --> "MVC Controller with views, using Entity Framework"
    Model class: WeatherForecast (Gruppe5.Models)
    DbContext class: Gruppe5.Data.Gruppe5Context
    Database provider: SQL Server (Dette er MS SQL LocalDB)
    
    - Kjørte disse kommander for å opprette database:
    add-migration initialmigration
    update-database
    
    - Tilslutt: lagt til andre modellene i database konteksten for å lage tabellene i databasen og utført migrasjon og oppdatering av databasen.
    
    Deloppgave 4 er ferdig.

---
+++ Data/Gruppe5Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gruppe5.Models;
+
+namespace Gruppe5.Data
+{
+    public class Gruppe5Context : DbContext
+    {
+        public Gruppe5Context(DbContextOptions<Gruppe5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gruppe5.Models.WeatherForecast> WeatherForecast { get; set; } = default!;
+        public DbSet<Gruppe5.Models.Observation> Observation { get; set; } = default!;
+        public DbSet<Gruppe5.Models.RootObject> RootObject { get; set; } = default!;
+        public DbSet<Gruppe5.Models.Level> Level { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfigurer Level som en keyless enhet
+            modelBuilder.Entity<Level>().HasNoKey();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Gruppe5.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Gruppe5Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Gruppe5Context") ?? throw new InvalidOperationException("Connection string 'Gruppe5Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
