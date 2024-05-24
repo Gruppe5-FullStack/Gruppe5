66b2e7e7689d9cd5cdba30322ed99ebc2b08a6c8
Gruppe5-FullStack <gruppe5fullstack@gmail.com>
2024-05-24 03:25:03 +0200

    Del 1: Deloppgave 3
    <Sett opp modeller for database i diagrammet>
    
    - Laget en modell for hver database i diagrammet.
    
    Deloppgaven 3 er ferdig.

---
+++ Models/Observation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gruppe5.Models
+{
+    public class Observation
+    {
+        [Key]
+        public Guid Elementid { get; set; }
+
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+
+        public string TimeOffset { get; set; }
+
+        public string TimeResolution { get; set; }
+
+        public int TimeSeriesId { get; set; }
+    }
+}
---
+++ Models/RootObject.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gruppe5.Models
+{
+    public class RootObject
+    {
+        [Key]
+        public Guid SourceId { get; set; }
+        public DateTime ReferenceTime { get; set; }
+
+        public ICollection<Observation> Observations { get; set; }
+    }
+}
